f0608da213f1c2e3465f210cc8046374a33f5e51
Gabriel Corrêa Diniz <gabriel.correa@twsolutions.com.br>
2020-05-28 22:38:04 -0300

    feat(Products): Added Modal for Products

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,76 +1,79 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxMaskModule } from 'ngx-mask';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthenticationComponent } from './authentication/authentication.component';
+import { CartComponent } from './cart/cart.component';
+import { DialogComponent, HomeProductsComponent } from './home-products/home-products.component';
+import { RegisterComponent } from './register/register.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 
-import { MatListModule } from '@angular/material/list';
-import { MatCardModule } from '@angular/material/card';
-import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatBadgeModule } from '@angular/material/badge';
-import { MatMenuModule } from '@angular/material/menu';
-import { HomeProductsComponent } from './home-products/home-products.component';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatDividerModule } from '@angular/material/divider';
-import { CartComponent } from './cart/cart.component';
-import { MatTableModule } from '@angular/material/table';
-import { RegisterComponent } from './register/register.component';
-import { NgxMaskModule, IConfig } from 'ngx-mask';
-
-// export const options: Partial<IConfig> | (() => Partial<IConfig>);
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    AuthenticationComponent,
-    ToolbarComponent,
-    HomeProductsComponent,
-    CartComponent,
-    RegisterComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    FormsModule,
-    ReactiveFormsModule,
-    NgxMaskModule.forRoot(),
+    declarations: [
+        AppComponent,
+        AuthenticationComponent,
+        ToolbarComponent,
+        HomeProductsComponent,
+        CartComponent,
+        RegisterComponent,
+        DialogComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        FormsModule,
+        ReactiveFormsModule,
+        NgxMaskModule.forRoot(),
 
-    MatListModule,
-    MatCardModule,
-    MatInputModule,
-    MatIconModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatBadgeModule,
-    MatMenuModule,
-    MatGridListModule,
-    MatDividerModule,
-    MatTableModule
-  ],
-  exports: [
-    MatListModule,
-    MatCardModule,
-    MatInputModule,
-    MatIconModule,
-    MatButtonModule,
-    MatToolbarModule,
-    MatBadgeModule,
-    MatMenuModule,
-    MatGridListModule,
-    MatDividerModule,
-    MatTableModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+        MatListModule,
+        MatCardModule,
+        MatInputModule,
+        MatIconModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatBadgeModule,
+        MatMenuModule,
+        MatGridListModule,
+        MatDividerModule,
+        MatTableModule,
+        MatDialogModule,
+        MatSnackBarModule
+    ],
+    exports: [
+        MatListModule,
+        MatCardModule,
+        MatInputModule,
+        MatIconModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatBadgeModule,
+        MatMenuModule,
+        MatGridListModule,
+        MatDividerModule,
+        MatTableModule,
+        MatDialogModule,
+        MatSnackBarModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
--- front-end/src/app/home-products/home-products.component.ts
+++ front-end/src/app/home-products/home-products.component.ts
@@ -1,15 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
-  selector: 'eniac-home-products',
-  templateUrl: './home-products.component.html',
-  styleUrls: ['./home-products.component.scss']
+    selector: 'eniac-home-products',
+    templateUrl: './home-products.component.html',
+    styleUrls: ['./home-products.component.scss']
 })
 export class HomeProductsComponent implements OnInit {
 
-  constructor() { }
+    constructor(
+        public dialog: MatDialog,
+        private snackBar: MatSnackBar
+    ) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
+    public viewProduct(): void {
+        const dialogRef = this.dialog.open(DialogComponent, {
+            width: '25%',
+            data: {}
+        });
+
+        dialogRef.afterClosed().subscribe(result => {
+            if (result) {
+                this.snackBar.open(
+                    'Produto Adicionado !',
+                    'Fechar', {
+                    duration: 2500,
+                    horizontalPosition: 'right',
+                    verticalPosition: 'top',
+                });
+            }
+        });
+    }
+
+}
+
+@Component({
+    selector: 'eniac-dialog',
+    template: `
+    <img mat-card-image src="https://material.angular.io/assets/img/examples/shiba2.jpg" alt="product">
+    <mat-dialog-content>
+        <h2>
+            Nome do Produto
+        </h2>
+        <small>
+            Preço R$ 25,99.
+        </small>
+    </mat-dialog-content>
+    <mat-dialog-actions>
+        <button mat-button (click)="close()">Adicionar ao Carrinho</button>
+        <button mat-button [mat-dialog-close]="true">Comprar</button>
+    </mat-dialog-actions>
+    `
+})
+export class DialogComponent {
+    constructor(
+        public dialogRef: MatDialogRef<DialogComponent>,
+        @Inject(MAT_DIALOG_DATA) public data: any
+    ) { }
+
+    public close(): void {
+
+        this.dialogRef.close(true);
+    }
 }
