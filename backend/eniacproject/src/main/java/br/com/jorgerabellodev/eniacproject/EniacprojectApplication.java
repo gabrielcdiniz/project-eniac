e02d2f339a2143277ea38975df00f59e67aefe1e
Jorge Rabello <jorge.rabello3@gmail.com>
2020-06-01 07:55:32 -0300

    feat: reformat file

--- backend/eniacproject/src/main/java/br/com/jorgerabellodev/eniacproject/EniacprojectApplication.java
+++ backend/eniacproject/src/main/java/br/com/jorgerabellodev/eniacproject/EniacprojectApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EniacprojectApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(EniacprojectApplication.class, args);
-  }
-
+    public static void main(String[] args) {
+        SpringApplication.run(EniacprojectApplication.class, args);
+    }
 }
